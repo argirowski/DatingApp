0df53b4abcd99d359c84b8bc5be42711e97cd158
Gjorgji Argirovski <argirovski.g@gmail.com>
2023-04-23 18:08:06 +0200

    Add files via upload

---
+++ API/Dtos/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength =4)]
+        public string Password { get; set; }
+    }
+}
